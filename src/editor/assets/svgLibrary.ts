50d7c82502b49ab99b1899b2b5f0a988a639e19d
Kilian Vivien <kilian.vivien@gmail.com>
2026-06-24 23:22:19 +0300

    Update heart SVG path

--- src/editor/assets/svgLibrary.ts
+++ src/editor/assets/svgLibrary.ts
@@ -40,7 +40,7 @@ export const SVG_LIBRARY: SvgLibraryItem[] = [
     nameKey: 'heart',
     category: 'engagement',
     keywords: 'heart like love favourite react',
-    svg: solid('<path d="M12 21s-7.5-4.6-10-9.2C.2 8.6 1.8 5 5.2 5c2 0 3.3 1.1 4 2.1.7-1 2-2.1 4-2.1 3.4 0 5 3.6 3.2 6.8C19.5 16.4 12 21 12 21z"/>'),
+    svg: solid('<path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54z"/>'),
   },
   {
     id: 'thumbsUp',
